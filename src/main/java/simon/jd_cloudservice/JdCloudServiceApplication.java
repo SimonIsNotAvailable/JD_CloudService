b1f96509eea9d29d732860d3d0a50db8f645f5de
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-06-03 05:00:36 +0500

    Initial commit

---
+++ src/main/java/simon/jd_cloudservice/JdCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package simon.jd_cloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdCloudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simon/jd_cloudservice/JdCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package simon.jd_cloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdCloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
