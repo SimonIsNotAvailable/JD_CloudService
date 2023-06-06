c0ad8036caaec9a6b8fe83e8c4631eca5e7b4a4e
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-06-06 21:21:07 +0500

    progress added 2

--- src/main/java/simon/jd_cloudservice/service/FileService.java
+++ src/main/java/simon/jd_cloudservice/service/FileService.java
@@ -2,8 +2,10 @@ package simon.jd_cloudservice.service;
 
 import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+
 public interface FileService {
-    public void uploadFile(byte[] content, String hash, String filename);
-    public void deleteFile(String hash);
-    public String downloadFile(String hash);
+    public void uploadFile(byte[] content, String hash, String filename) throws IOException;
+    public void deleteFile(String hash) throws IOException;
+    public Resource downloadFile(String hash);
 }
--- src/main/java/simon/jd_cloudservice/service/FileServiceImpl.java
+++ src/main/java/simon/jd_cloudservice/service/FileServiceImpl.java
@@ -1,18 +1,38 @@
 package simon.jd_cloudservice.service;
 
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
 public class FileServiceImpl implements FileService{
-    @Override
-    public void uploadFile(byte[] content, String hash, String filename) {
+    private String storage;     // application.properties
 
+    @Override
+    public void uploadFile(byte[] content, String hash, String filename) throws IOException {
+        Path path = Paths.get(storage, hash);
+        Path file = Files.createFile(path);
+
+        try (FileOutputStream stream = new FileOutputStream(file.toString())) {
+            stream.write(content);
+        }
     }
 
     @Override
-    public void deleteFile(String hash) {
-
+    public void deleteFile(String hash) throws IOException {
+        Path path = Paths.get(storage, hash);
+        Files.delete(path);
     }
 
     @Override
-    public String downloadFile(String hash) {
-        return null;
+    public Resource downloadFile(String hash) {
+        Path path = Paths.get(storage, hash);
+        return new PathResource(path);
     }
 }
