c833296a420edd62efa6030873d105139977ca70
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-06-12 15:27:33 +0500

    Mapping added

--- src/main/java/simon/jd_cloudservice/controller/CloudController.java
+++ src/main/java/simon/jd_cloudservice/controller/CloudController.java
@@ -1,12 +1,45 @@
 package simon.jd_cloudservice.controller;
 import lombok.*;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import simon.jd_cloudservice.dto.FileDto;
+import simon.jd_cloudservice.dto.FileInfoDto;
 import simon.jd_cloudservice.service.CloudService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class CloudController {
     private CloudService cloudService;
 
+    @PostMapping("/file")
+    @ResponseStatus(HttpStatus.OK)
+    public void uploadFile(@RequestParam String name, @RequestBody MultipartFile file) {
+        cloudService.saveFile(name, file);
+    }
+    @GetMapping ( "/file")
+    @ResponseStatus(HttpStatus.OK)
+    public FileDto downloadFile(@RequestParam String name){
+        return cloudService.downloadFile(name);
+    }
 
+    @DeleteMapping("/file")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFile(@RequestParam String name) {
+        cloudService.deleteFile(name);
+    }
+
+    @PutMapping("/file")
+    @ResponseStatus(HttpStatus.OK)
+    public void editFile(@RequestParam String name, @RequestBody String newName) {
+        cloudService.editFilename(name, newName);
+    }
+
+    @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
+    public List<FileInfoDto> getFilesList (@RequestParam int limit){
+        return cloudService.getFiles(limit);
+    }
 }
