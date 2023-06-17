beed5d440e60b97ff70fcdd3f040ce65274d3792
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-06-17 16:25:46 +0500

    CloudServiceTest blank methods added

--- src/test/java/simon/jd_cloudservice/CloudServiceTest.java
+++ src/test/java/simon/jd_cloudservice/CloudServiceTest.java
@@ -1,4 +1,43 @@
 package simon.jd_cloudservice;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CloudServiceTest {
+
+    @Test
+    public void testSaveFileWhenExistedFilenameThenThrowEx() {
+
+    }
+
+    @Test
+    public void testSaveFileWhenFileIsNotAttachedThenThrowEx() {
+
+    }
+
+    @Test
+    public void testDeleteFileWhenNotExistingFileThenThrowEx() {
+
+    }
+
+    @Test
+    public void testDeleteFileWhenCorrectDataThenDeleteFile(){
+
+    }
+    @Test
+    public void testDownloadFileWhenNotExistingFileThenThrowEx(){
+
+    }
+
+    @Test
+    public void testDownloadFileWhenCorrectDataThenReturnFileDto() {
+
+    }
+
+    @Test
+    public void testEditFilenameWhenCorrectDataThenSaveWithNewName() {
+
+    }
+
 }
