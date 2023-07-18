85d21d6cca5fc7d3d136cda9b4e23ad022d3ecf2
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-07-18 21:40:39 +0500

    AuthController to AuthService extended

--- src/main/java/simon/jd_cloudservice/controller/AuthController.java
+++ src/main/java/simon/jd_cloudservice/controller/AuthController.java
@@ -1,39 +1,22 @@
 package simon.jd_cloudservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import simon.jd_cloudservice.dto.LoginDto;
-import simon.jd_cloudservice.exception.WrongDataException;
-import simon.jd_cloudservice.security.UtilsJwt;
+import simon.jd_cloudservice.service.AuthService;
 
-import java.util.Collections;
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UtilsJwt jwtUtil;
-
-    private final AuthenticationManager authManager;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public Map<String, Object> loginHandler(@RequestBody LoginDto body) {
-        try {
-            UsernamePasswordAuthenticationToken authInputToken =
-                    new UsernamePasswordAuthenticationToken(body.getLogin(), body.getPassword());
-
-            authManager.authenticate(authInputToken);
-            String token = jwtUtil.generateToken(body.getLogin());
-            return Collections.singletonMap("auth-token", token);
-
-        } catch (AuthenticationException exception) {
-            throw new WrongDataException("Bad credentials " + exception);
-        }
+        return authService.login(body);
     }
 }
